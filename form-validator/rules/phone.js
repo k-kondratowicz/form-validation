ce19449754429b8d047fc8a0ce6ff57e78280541
Kamil Kondratowicz <kamil.kondratowicz@owlsdeprtment.com>
2021-11-06 22:51:56 +0100

    removed comment

--- form-validator/rules/phone.js
+++ form-validator/rules/phone.js
@@ -1,6 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export, no-useless-escape
 export const phone = (value) => /^[0-9]{1,}[0-9\-]{3,15}$/.test(value) || 'Provide a valid phone number';
-// export const phone = {
-// 	validator: (value) => /^[0-9]{1,}[0-9\-]{3,15}$/.test(value),
-// 	message: 'Provide a valid phone number'
-// };
